bd637d3f695c4e382c02bc6bcf57f48c1ebf0939
Deepak Badu <dips.badu@gmail.com>
2021-03-26 11:26:01 +0545

    box move random postion

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-function move(){
-  let box = document.getElementById("box")
-  if(box.style.left.replace("px","") < 420 && check == true){
-    moveRight(box);
-  }else if(box.style.left.replace("px","") == 420 || check == false) {
-    if(box.style.left.replace("px","") > 0){
-      check = false
-      moveleft(box)
-    } else if(box.style.left.replace("px","") == 0){
-      check = true
+var box;
+var maxBoxX = 500 - 50;
+var maxBoxY = 300 - 50;
+var speed = 5;
+window.onload = init
+
+function init() {
+  box = document.getElementById("box");
+  box.posX = box.offsetLeft;
+  box.posY = box.offsetTop;
+  box.velX = speed;
+  box.velY = speed;
+  box.move = true;
+  setInterval(moveBox,30);
+}
+
+function moveBox() {
+  if (box.move) {
+    box.posX += box.velX;
+    box.posY += box.velY;
+
+    if(box.posX <= 0) {
+      box.posX = 0;
+      box.velX = -box.velX;
     }
+
+    if(box.posX >= maxBoxX) {
+      box.posX = maxBoxX;
+      box.velX = -box.velX;
+    }
+
+    if(box.posY <= 0){
+      box.posY = 0;
+      box.velY = -box.velY;
+    }
+    
+    if(box.posY >= maxBoxY) {
+      box.posY = maxBoxY;
+      box.velY = -box.velY;
+    }
+    box.style.left = box.posX + "px";
+    box.style.top  = box.posY + "px";
   }
 }
-function moveRight(box){
-  if(box.style.left != ""){
-    let temp = box.style.left.replace("px","");
-    temp = parseInt(temp);
-    box.style.left = temp+1+"px";
-  }else{
-    box.style.left = "1px";
-  }
-}
-function moveleft(box){
-  if(box.style.left != ""){
-    let temp = box.style.left.replace("px","")
-    temp = parseInt(temp)
-    box.style.left = temp-1 + "px"
-  } else {
-    box.style.left = "1px"
-  }
-}
-var check = true;
-setInterval(move,20);
